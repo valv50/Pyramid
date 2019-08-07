69a6c35debd961c9ddebd0cb9f00e534bfa0bde7
valv50 <valv50@gmail.com>
2019-08-08 02:31:42 +0300

    fghdf

---
+++ Pyramid/DataProcessing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pyramid
+{
+    public class PyramidProcessing
+    {
+        private bool _isEven;
+
+        private int[][] _initialData;
+
+        private List<(int sum, List<(int index, int value)>)> _results;          
+
+        PyramidProcessing(int[][] initialData)
+        {
+            _initialData = initialData;
+
+            _results = new List<(int sum, List<(int index, int value)>)>();
+        }
+
+        public void Process()
+        {
+            for (int i1 = 1; i1 <= _initialData.Length; i1++)
+            {
+                if (i1 == 1)
+                {
+                    _isEven = _initialData[i1 - 1][0] % 2 == 0;
+                }
+
+                for (int i2 = 1; i2 <= _initialData[i1 - 1].Length)
+                {
+                    if (_initialData[i1 - 1][i2 - 1] % 2 ==
+                        (_isEven ? 0 : 1))
+                    {
+
+                    }
+
+                }
+
+                _isEven = !_isEven;
+            }
+        }
+    }
+}
---
+++ Pyramid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] initialData =
+                new int[][]
+                { 
+                    new int[] { 1 }
+                    , new int[] { 8, 9 }
+                    , new int[] { 1, 5, 9}
+                    , new int[] { 4, 5, 2, 3}
+                };
+        }
+    }
+}
