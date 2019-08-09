4f596986c5ac00101e99429cc0e7c58642845fa0
valv50 <44941227+valv50@users.noreply.github.com>
2019-08-09 03:13:06 +0300

    Test

--- Pyramid/DataProcessing.cs
+++ Pyramid/DataProcessing.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Pyramid
@@ -10,36 +11,114 @@ namespace Pyramid
 
         private int[][] _initialData;
 
-        private List<(int sum, List<(int index, int value)>)> _results;          
+        private List<(int sum, int lastIndex, int lastLevel, List<(int index, int value)> path)> _results;          
 
-        PyramidProcessing(int[][] initialData)
+        public PyramidProcessing(int[][] initialData)
         {
             _initialData = initialData;
 
-            _results = new List<(int sum, List<(int index, int value)>)>();
+            _results = new List<(int, int, int, List<(int index, int value)>)>();
         }
 
-        public void Process()
+        public (int sum, List<(int index, int value)> path) Process()
         {
             for (int i1 = 1; i1 <= _initialData.Length; i1++)
             {
                 if (i1 == 1)
                 {
                     _isEven = _initialData[i1 - 1][0] % 2 == 0;
+
+                    FillFirstLevel();
                 }
-
-                for (int i2 = 1; i2 <= _initialData[i1 - 1].Length)
+                else
                 {
-                    if (_initialData[i1 - 1][i2 - 1] % 2 ==
-                        (_isEven ? 0 : 1))
-                    {
-
-                    }
-
+                    FillNewLevel(i1, _isEven);
                 }
 
                 _isEven = !_isEven;
             }
+
+            (int sum, List<(int index, int value)> path) result;
+
+            result = _results.OrderByDescending(o => o.sum)
+                .Select(s => (s.sum, s.path))
+                .First();
+
+            return result;
+        }
+
+        private void FillFirstLevel()
+        {
+            (int sum, int lastIndex, int lasLevel, List<(int index, int value)> path) resultItem;
+
+            resultItem = (_initialData[0][0]
+                , 1
+                , 1
+                , new List<(int index, int value)>());
+
+            resultItem.path.Add((1, _initialData[0][0]));
+
+            _results.Add(resultItem);
+        }
+
+        private void FillNewLevel(int level, bool isEven)
+        {
+            List<(int sum, int lastIndex, int lastLevel, List<(int index, int value)> path)> newItems;
+
+            newItems = new List<(int sum, int lastIndex, int lastLevel, List<(int index, int value)> path)>();
+
+            foreach ((int sum, int lastIndex, int lastLevel, List<(int index, int value)> path) resultItem in _results)
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    int index = resultItem.lastIndex + i;
+
+                    if ((_initialData[level - 1][index - 1] % 2 == 0) == isEven)
+                    {
+                        newItems.Add(CopyItem(resultItem, index, _initialData[level - 1][index - 1]));
+                    }
+                }                
+            }
+
+            _results.RemoveAll(r => r.lastLevel < level);
+
+            var highestInGroups =
+                from n in newItems
+                group n by n.lastIndex into itemgrp
+                let topSum = itemgrp.Max(x => x.sum)
+                select new
+                {
+                    lastIndex = itemgrp.Key,
+                    maxSum = topSum
+                };
+            
+            _results.AddRange(
+                from newItem in newItems
+                join highestInGroup in highestInGroups on
+                    new { newItem.lastIndex, newItem.sum } 
+                    equals new { highestInGroup.lastIndex, sum = highestInGroup.maxSum}
+                select newItem);
+        }
+
+        private (int sum, int lastIndex, int lastLevel, List<(int index, int value)> path) 
+            CopyItem((int sum, int lastIndex, int lastLevel, List<(int index, int value)> path) resultItem
+            , int index, int value)
+        {
+            (int sum, int lastIndex, int lastLevel, List<(int index, int value)> path) newItem;
+
+            List<(int index, int value)> newPath = new List<(int index, int value)>();
+
+            newPath.AddRange(resultItem.path);
+
+            newPath.Add((index, value));
+
+            newItem = (value + resultItem.sum
+                , index
+                , resultItem.lastLevel + 1
+                , newPath);
+
+            return newItem;
+
         }
     }
 }
--- Pyramid/Program.cs
+++ Pyramid/Program.cs
@@ -8,12 +8,36 @@ namespace Pyramid
         {
             int[][] initialData =
                 new int[][]
-                { 
-                    new int[] { 1 }
-                    , new int[] { 8, 9 }
-                    , new int[] { 1, 5, 9}
-                    , new int[] { 4, 5, 2, 3}
+                {
+                    new int[] { 215 }
+                    , new int[] { 192, 124 }
+                    , new int[] { 117, 269, 442 }
+                    , new int[] { 218, 836, 347, 235 }
+                    , new int[] { 320, 805, 522, 417, 345 }
+                    , new int[] { 229, 601, 728, 835, 133, 124 }
+                    , new int[] { 248, 202, 277, 433, 207, 263, 257 }
+                    , new int[] { 359, 464, 504, 528, 516, 716, 871, 182 }
+                    , new int[] { 461, 441, 426, 656, 863, 560, 380, 171, 923 }
+                    , new int[] { 381, 348, 573, 533, 448, 632, 387, 176, 975, 449 }
+                    , new int[] { 223, 711, 445, 645, 245, 543, 931, 532, 937, 541, 444 }
+                    , new int[] { 330, 131, 333, 928, 376, 733, 017, 778, 839, 168, 197, 197 }
+                    , new int[] { 131, 171, 522, 137, 217, 224, 291, 413, 528, 520, 227, 229, 928 }
+                    , new int[] { 223, 626, 034, 683, 839, 052, 627, 310, 713, 999, 629, 817, 410, 121 }
+                    , new int[] { 924, 622, 911, 233, 325, 139, 721, 218, 253, 223, 107, 233, 230, 124, 233}
                 };
+
+            PyramidProcessing pyramidProcessing = new PyramidProcessing(initialData);
+
+            var result = pyramidProcessing.Process();
+
+            Console.WriteLine($"Sum = {result.sum}");
+
+            foreach ((int index, int value) pathStep in result.path)
+            {
+                Console.WriteLine($"Index = {pathStep.index}, Value = {pathStep.value}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
